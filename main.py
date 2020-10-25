216a789244ee7e7e13124eade579f1b763e7ef86
lkarvec <lkarvec@gmail.com>
2020-10-24 20:46:17 -0500

    Updating with some copied code that needs edited to actually suit our project

--- main.py
+++ main.py
@@ -1,8 +1,116 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Oct 24 19:37:48 2020
+What does the program do?
+    Takes a deck provided and simulates play with the starting hand to
+    determine if the starting hand should be mulliganed.
 
-@author: bigpu
+
+
+
+@author: Alexander Mains
 """
+import random
 
+class Battlefield():
+    
+    
+class Land():
+
+class Card(object):
+    def __init__(self, card):
+        self.name = card.name
+        self.value = card.value
+        
+
+    # Implementing build in methods so that you can print a card object
+    def __unicode__(self):
+        return self.show()
+    def __str__(self):
+        return self.show()
+    def __repr__(self):
+        return self.show()
+        
+    def show(self):
+
+        return self
+
+
+class Deck(object):
+    def __init__(self):
+        self.cards = []
+        self.build()
+
+    # Display all cards in the deck
+    def show(self):
+        for card in self.cards:
+            print (card.show())
+
+    # Generate 52 cards
+    def build(self):
+        self.cards = []
+        for suit in ['Hearts', 'Clubs', 'Diamonds', 'Spades']:
+            self.cards.append(Card(suit, val))
+
+    # Shuffle the deck
+    def shuffle(self, num=1):
+        length = len(self.cards)
+        for _ in range(num):
+            # This is the fisher yates shuffle algorithm
+            for i in range(length-1, 0, -1):
+                randi = random.randint(0, i)
+                if i == randi:
+                    continue
+                self.cards[i], self.cards[randi] = self.cards[randi], self.cards[i]
+            # You can also use the build in shuffle method
+            # random.shuffle(self.cards)
+
+    # Return the top card
+    def deal(self):
+        return self.cards.pop()
+
+
+class Player(object):
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
+        self.life = 20
+
+    def sayLife(self):
+        print ("{} has {} life left".format(self.name,self.life))
+        return self
+
+    # Draw n number of cards from a deck
+    # Returns true in n cards are drawn, false if less then that
+    def draw(self, deck, num=1):
+        for _ in range(num):
+            card = deck.deal()
+            if card:
+                self.hand.append(card)
+            else: 
+                return False
+        return True
+
+    # Display all the cards in the players hand
+    def showHand(self):
+        print ("Player's hand: {}".format(self.hand))
+        return self
+
+    def discard(self):
+        return self.hand.pop()
+
+# Test making a Card
+# card = Card('Spades', 6)
+# print card
+
+# Test making a Deck
+myDeck = Deck()
+myDeck.shuffle()
+# deck.show()
+
+PlayerOne = Player("One")
+
+PlayerOne.sayHello()
+PlayerOne.draw(myDeck, 7)
+PlayerOne.showHand()
 
