e9bd059bcc5505205cb929ab8dd343a3d45342ca
lkarvec <lkarvec@gmail.com>
2020-11-25 20:12:43 -0600

    Update main.py
    Eliminated Phases, place logging features in.

--- main.py
+++ main.py
@@ -14,7 +14,13 @@ import random
 #imported models
 from numpy import loadtxt
 from enum import Enum
+import logging
+import os
 
+import game
+import mcts
+import player
+import deck
 
 
 """
@@ -39,32 +45,6 @@ class Battlefield(object):
         self.graveyardOne = [] #Player One Graveyard
         self.graveyardTwo = [] #Player Two graveyard
         
-class Phases(Enum):
-    BEGINNING_PHASE = 1
-    UNTAP_STEP = 2
-    UPKEEP_STEP = 3
-    DRAW_STEP = 4
-    MAIN_PHASE_PRE_COMBAT = 5
-    COMBAT_PHASE = 6
-    BEGINNING_OF_COMBAT_STEP = 7
-    DECLARE_ATTACKERS_STEP = 8
-    DECLARE_BLOCKERS_STEP = 9
-    DECLARE_BLOCKERS_STEP_509_2 = 10
-    COMBAT_DAMAGE_STEP_510_1c = 11
-    COMBAT_DAMAGE_STEP = 12
-    END_OF_COMBAT_STEP = 13
-    MAIN_PHASE_POST_COMBAT = 14
-    ENDING_PHASE = 15
-    END_STEP = 16
-    CLEANUP_STEP = 17
-
-    def next(self):
-        cls = self.__class__
-        members = list(cls)
-        index = members.index(self) + 1
-        if index >= len(members):
-            index = 0
-        return members[index]
 
 class Land():
     def __init__(self, name, value, color, rulesText, sickness):
@@ -204,7 +184,90 @@ class Player(object):
     def discard(self):
         return self.hand.pop()
     
-    
+
+
+
+def configure_logging():
+    """
+    Configures logging
+    :return:
+    """
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.DEBUG)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(logging.Formatter("[%(levelname)-5.5s]  %(message)s"))
+    file_handler = logging.FileHandler("open_mtg.log", 'a', 'utf-8')
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(logging.Formatter("%(asctime)s  [%(levelname)-5.5s]  %(message)s"))
+
+    # code for later when stream output level can be set by command line argument
+    """
+    if args.verbose:
+        stream_handler.setLevel(logging.INFO)
+    elif args.silent:
+        stream_handler.setLevel(logging.ERROR)
+    elif args.debug:
+        stream_handler.setLevel(logging.DEBUG)
+    else:
+        stream_handler.setLevel(logging.WARNING)
+    """
+
+    root_logger.addHandler(stream_handler)
+    root_logger.addHandler(file_handler)
+
+
+def start_games(amount_of_games):
+    player_a_wins = 0
+    player_b_wins = 0
+    games_played = 0
+
+    logging.info("Starting Open MTG. Playing {0} games".format(amount_of_games))
+    for i in range(amount_of_games):
+        gold_deck = deck.get_8ed_core_gold_deck()
+        silver_deck = deck.get_8ed_core_silver_deck()
+        current_game = game.Game([player.Player(gold_deck), player.Player(silver_deck)])
+        current_game.start_game()
+
+        if current_game.active_player.index == 0:
+            logging.info("Gold player starts game")
+        else:
+            logging.info("Silver player starts game")
+        while not current_game.is_over():
+            if current_game.player_with_priority.index is 1:
+                move = current_game.player_with_priority.determine_move(method="random", game=current_game)
+            else:
+                # move = game.player_with_priority.determine_move(method="random", game=game)
+                if len(current_game.get_moves()) == 1:
+                    move = current_game.get_moves()[0]
+                else:
+                    move = mcts.uct(current_game, itermax=5)
+
+            current_game.make_move(move, False)
+
+        if current_game.players[1].has_lost:
+            player_a_wins += 1
+        elif current_game.players[0].has_lost:
+            player_b_wins += 1
+        games_played += 1
+        logging.info("Game {0} is over! current standings: "
+                     "{1} - {2}".format(games_played, player_a_wins, player_b_wins))
+
+    logging.info("Player A won {0} out of {1}".format(player_a_wins, games_played))
+    logging.info("Player B won {0} out of {1}".format(player_b_wins, games_played))
+    logging.info("Quitting Open MTG{0}{0}".format(os.linesep))
+
+
+if __name__ == "__main__":
+    try:
+        configure_logging()
+        start_games(2)
+    except SystemExit:
+        pass
+    except KeyboardInterrupt:
+        logging.error("Open-mtg stopped by Keyboard Interrupt{0}{0}".format(os.linesep))
+    except:
+        logging.exception("Unexpected exception")
 
 #The x needs to be the array of inputs whilst Y needs to be array of outputs
 #x = []
